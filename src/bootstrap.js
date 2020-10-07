9b929ffed03e6da701610930fd707f1c67a0093e
zinvergo57 <zachary.coding@gmail.com>
2020-10-07 09:17:15 -0500

    update

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,3 +3,7 @@ import * as classChallenge from './classChallenge';
 console.log(classChallenge.greeting)
 
 console.log(classChallenge.sumArray(7, 10, 4))
+
+console.log(classChallenge.stringUpper('death is not an escape'))
+
+console.log(classChallenge.arrayWeave())
--- src/classChallenge.js
+++ src/classChallenge.js
@@ -9,3 +9,23 @@ export function sumArray(a, b, c) {
     return sum
 }
 
+export function stringUpper(capString) {
+    return capString.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+export function arrayWeave (){
+    function splicer(array, element, index) {
+        array.splice(index * 2, 0, element);
+        return array;
+    }
+    function weave(arr1,arr2) {
+        return arr1.reduce(splicer, arr2.slice());
+    }
+
+    let arr1 = ['a', 'b', 'c', 'd']
+    let arr2 = [1, 2, 3, 4]
+
+    let solution = weave(arr1, arr2)
+
+    console.log(solution)
+}
